d685b3b7eeda4a73efe9bd30aeacf7186ea20e13
Michael Ovakimian <mov.express17@gmail.com>
2023-03-23 19:36:29 +0300

    Empty master branch

--- HW1/include/validations.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <boost/program_options.hpp>
-#include <iostream>
-
-namespace po = boost::program_options;
-
-bool ValidateAndStore(const po::variables_map& vm, const std::string& field, std::string& storage);

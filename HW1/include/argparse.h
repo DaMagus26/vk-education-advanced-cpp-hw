6afe9f17418581620cdca86bf0ea1db4684f1401
Michael Ovakimian <mov.express17@gmail.com>
2023-03-26 17:36:59 +0300

    cleared branch

--- HW1/include/argparse.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <map>
-#include <string>
-
-std::map<std::string, std::string> ParseCmdLineOptions(int argc, const char** argv);
--- HW1/include/validations.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <map>
-#include <string>
-
-bool ValidateAndStore(std::map<std::string, std::string>& vm, const std::string& field, std::string& storage);
